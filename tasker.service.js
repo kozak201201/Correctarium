14e98950108c78a68f582869d8477acc5abaef8b
kozak201201 <kozak201201@gmail.com>
2021-12-13 12:14:06 +0200

    add methods to tasker.service.js

---
+++ tasker.service.js
@@ -0,0 +1,15 @@
+class TaskerService {
+    useTaskToText(countOfWords, language, mimeType, startDate=new Date()) {
+    }
+
+    priceCalculate(countOfWords, language, mimeType) {
+    }
+
+    leadTimeCalculate(countOfWords, language, mimeType) {
+    }
+
+    deadlineDateCalculate(startDate, leadTime) {
+    }
+}
+
+module.exports = TaskerService;
