b17620125b3107476bcd77abcce6d185ccf93e27
kozak201201 <kozak201201@gmail.com>
2021-12-13 12:24:31 +0200

    edit tasker.service.spec.js

--- tasker/tasker.service.spec.js
+++ tasker/tasker.service.spec.js
@@ -1,5 +1,40 @@
 const Tasker = require('./tasker.service');
+
+let uk = {
+    pricePerWord: 0.05,
+    minimumPrice: 50,
+}
+
+let ru = {
+    pricePerWord: 0.05,
+    minimumPrice: 50,
+}
+
+let en = {
+    pricePerWord: 0.12,
+    minimumPrice: 120,
+}
+
+const fileFactorMap = new Map();
+fileFactorMap.set('doc', 0);
+fileFactorMap.set('docx', 0);
+fileFactorMap.set('rtf', 0);
+fileFactorMap.set('other', 0.2);
+
+const pricePerWordMap = new Map();
+pricePerWordMap.set('uk', uk);
+pricePerWordMap.set('ru', ru);
+pricePerWordMap.set('en', en);
+
+const wordsPerHourMap = new Map();
+wordsPerHourMap.set('uk', 1333);
+wordsPerHourMap.set('ru', 1333);
+wordsPerHourMap.set('en', 333);
+
 const tasker = new Tasker();
+tasker.fileFactorMap = fileFactorMap;
+tasker.pricePerWordMap = pricePerWordMap;
+tasker.wordsPerHourMap = wordsPerHourMap;
 
 describe('Tasker: priceCalculate', function () {
     test('priceCalculate should return correct price', () => {
